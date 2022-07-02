e450e09240add73248c52be0532b88ee720830c1
Adepoju Lateef <ademustexcel@gmail.com>
2022-07-02 19:12:29 +0100

    password changed to a new password

--- backend/dbsetup.py
+++ backend/dbsetup.py
@@ -3,5 +3,5 @@ import os
 load_dotenv()
 DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
 DB_USER = os.getenv('DB_USER', 'postgres')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'lateef')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'lateefbabatunde')
 DB_NAME = os.getenv('DB_NAME', 'trivia_udacity')
