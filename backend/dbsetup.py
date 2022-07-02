6781e089e9b2b7b5c2108275dcc96c0b87cf734e
Adepoju Lateef <ademustexcel@gmail.com>
2022-07-02 19:13:44 +0100

    auth issue fixed

--- backend/dbsetup.py
+++ backend/dbsetup.py
@@ -3,5 +3,5 @@ import os
 load_dotenv()
 DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
 DB_USER = os.getenv('DB_USER', 'postgres')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'lateefbabatunde')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'lateefbabatund')
 DB_NAME = os.getenv('DB_NAME', 'trivia_udacity')
